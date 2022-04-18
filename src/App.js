c45bb0e46c9eb299f0614a6313dc2cc828a0d3ea
KIMDAEHEE <57423919+Kim-Daehee123@users.noreply.github.com>
2022-04-18 14:50:11 +0900

    DiaryList
    To modify list data

--- src/App.js
+++ src/App.js
@@ -21,16 +21,23 @@ function App() {
     setDate([newItem, ...date]);
   };
 
-  const onDelete = (targetId) => {
+  const onRemove = (targetId) => {
     console.log(`${targetId}가 삭제되었습니다`);
     const newDiaryList = date.filter((it) => it.id !== targetId);
     setDate(newDiaryList);
   };
 
+  const onEdit = (targetId, newContent) => {
+    setDate(
+      date.map((it) =>
+        it.id === targetId ? { ...it, content: newContent } : it
+      )
+    );
+  };
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate} />
-      <DiaryList onDelete={onDelete} diaryList={date} />
+      <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={date} />
     </div>
   );
 }
